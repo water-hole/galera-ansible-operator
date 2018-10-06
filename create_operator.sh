2353ac54244a8a05a12a338a89b0a01efe144104
James Cammarata <jimi@sngx.net>
2018-10-06 15:14:43 -0500

    Split script to create/delete environment script

--- create_operator.sh
+++ create_operator.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 docker build -t galera-ansible-operator:latest .
-kubectl delete -f deploy/cr.yaml
-kubectl delete -f deploy/operator.yaml
 kubectl create -f deploy/operator.yaml
 kubectl create -f deploy/cr.yaml
 
