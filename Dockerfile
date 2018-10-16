20d6b16a1798d42172d5ca43e878290774f99db3
James Cammarata <jimi@sngx.net>
2018-10-16 11:43:42 -0500

    Reorganizing everything into a single role and lots of fixes
    * Adding lots of GCE support

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM ansible-operator
 
-COPY ansible/* ${HOME}/
+COPY ansible/roles/ ${HOME}/roles
+COPY playbook.yaml ${HOME}/playbook.yaml
 COPY watches.yaml ${HOME}/watches.yaml
+
--- create_operator.sh
+++ create_operator.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker build -t galera-ansible-operator:latest .
+#docker build -t gcr.io/consummate-atom-218703/galera-ansible-operator:latest .
+docker build -t gcr.io/consummate-atom-218703/galera-ansible-operator:latest .
+docker push gcr.io/consummate-atom-218703/galera-ansible-operator:latest
 kubectl create -f deploy/operator.yaml
 kubectl create -f deploy/cr.yaml
 
