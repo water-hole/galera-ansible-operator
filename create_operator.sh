8c6a0f7b0e394f8c39997fb31b147b65c2b64f92
James Cammarata <jimi@sngx.net>
2018-10-25 12:31:16 -0500

    Changes required to get things working on OpenShift

--- create_operator.sh
+++ create_operator.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-#docker build -t gcr.io/consummate-atom-218703/galera-ansible-operator:latest .
-docker build -t gcr.io/consummate-atom-218703/galera-ansible-operator:latest .
-docker push gcr.io/consummate-atom-218703/galera-ansible-operator:latest
+docker build -t docker-registry-default.router.default.svc.cluster.local/test/galera-ansible-operator:latest .
+docker push docker-registry-default.router.default.svc.cluster.local/test/galera-ansible-operator:latest
 kubectl create -f deploy/operator.yaml
 kubectl create -f deploy/cr.yaml
 
